e1fdf76515378d0d1d1d1eaac9d738d6e047c48d
keithhuddleston <kdhuddle@ksu.edu>
2017-06-09 09:59:13 -0500

    updated_plots/crosssectiondict

--- Xsection/xsectiondict.py
+++ Xsection/xsectiondict.py
@@ -1,38 +1,77 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 import matplotlib.pyplot as plt
-mo = ["Th_231", "Th_232", "Th_233", 
-      "Pa_231", "Pa_232", "Pa_233",
-      "U_232", "U_233", "U_234", "U_235", "U_236", "U_237", "U_238", "U_239",
-      "Np_237", "Np_238", "Np_239", 
-      "Pu_238", "Pu_239", "Pu_240",  "Pu_241", "Pu_242", "Pu_243",
-      "Am_241", "Am_242_metastable", "Am_242", "Am_243", "Am_244",
-      "Cm_242", "Cm_243", "Cm_244"] 
-dict = {}
-for i in mo:
-    energy = []
-    xsection = []
-    dict[i] = {"capture":None,"fission":None}
-    file = open(i + "_capture" + ".txt","r")
-    for line in file.readlines():
-        energy.append(line.split(",")[0])
-        xsection.append(line.split(",")[1])
-    dict[i]["capture"] = [energy,xsection]
-    energy = []
-    xsection = []
-    file = open(i + "_fission" + ".txt","r")
-    for line in file.readlines():
-        energy.append(line.split(",")[0])
-        xsection.append(line.split(",")[1])
-    dict[i]["fission"] = [energy,xsection]      
-#dict["U_235"]["capture"][0] gives the energy list of U_235 for capture
-#dict["U_235"]["fission"][1] gives the xsection list of U_235 for fission
-plt.plot(dict["U_238"]["capture"][0][1::],dict["U_238"]["capture"][1][1::])
-plt.yscale("log")
-plt.xscale("log")
-plt.ylabel("Cross Section(b)")
-plt.xlabel("Incedent Neutron Energy(eV)")
+import numpy as np
+
+def xsd(isotope):
+    """
+    Creates a dictionary containing both the fission and capture cross sections
+    of various isotopes
+    """
+    
+    #mo contains isotope names to be contained in the dictionary
+    mo = [
+        "Th_231", "Th_232", "Th_233", 
+        "Pa_231", "Pa_232", "Pa_233",
+        "U_232", "U_233", "U_234", "U_235", "U_236", "U_237", "U_238", "U_239",
+        "Np_237", "Np_238", "Np_239", 
+        "Pu_238", "Pu_239", "Pu_240",  "Pu_241", "Pu_242", "Pu_243",
+        "Am_241", "Am_242_metastable", "Am_242", "Am_243", "Am_244",
+        "Cm_242", "Cm_243", "Cm_244"
+        ] 
+    
+    #Checks that input is contained in mo
+    assert isotope in mo, "Isotope not considered"
+    
+    #d contains the fission and capture cross section information.
+    d = {}
+    for i in mo:
+        energy = []
+        xsection = []
+        d[i] = {"capture":None,"fission":None}
+        file = open(i + "_capture" + ".txt","r")
+        for line in file.readlines()[1:]:
+            energy.append(float(line.split(",")[0]))
+            xsection.append(float(line.split(",")[1]))
+        d[i]["capture"] = [energy,xsection]
+        energy = []
+        xsection = []
+        file = open(i + "_fission" + ".txt","r")
+        for line in file.readlines()[1:]:
+            energy.append(float(line.split(",")[0]))
+            xsection.append(float(line.split(",")[1]))
+        d[i]["fission"] = [energy,xsection]      
+    return(d)
+    
+def wxs(isotope,mode):
+    """
+    Calculates the weighted cross section for a triga reactor, as well 
+    as plots cross section vs energy
+    """   
+    
+    d = xsd(isotope)
+    
+    #Plots the cross section for the respective decay mode vs. energy.
+    plt.plot(d[isotope][mode][0],d[isotope][mode][1])
+    plt.yscale("log")
+    plt.xscale("log")
+    plt.ylabel("Cross Section(b)")
+    plt.xlabel("Incedent Neutron Energy(eV)") 
+    plt.show()
+    plt.close()
+    
+    #Weighted cross section.
+    E = np.loadtxt("energy_range")
+    phi = np.loadtxt("flux")
+    sigb = np.trapz(phi,E)
+    sigx = np.interp(E,d[isotope][mode][0],d[isotope][mode][1])
+    phisig = []
+    for i in range(0,1000):
+        phisig.append(phi[i]*sigx[i]) 
+    sigt = np.trapz(phisig, E)
+    wsig = sigt/sigb
+    return(wsig)
+
+
+
+
+
+
--- plots.py
+++ plots.py
@@ -1,16 +1,15 @@
-# -*- coding: utf-8 -*-
 """
 Created on Wed May 24 13:30:11 2017
-
-@author: keith
 """
 import numpy as np
-#np.set_printoptions(threshold=np.inf)
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
+
 #Isotopes considered are those listed in Neutron Physics by Paul Reuss in figures 12.2 and 12.3.
 #Halflife Data from Chart of the Nuclides 17th Edition.
-hl = {#Contains the half lives of varius isotopes.
+
+#Contains the half lives of varius isotopes.
+hl = {
     "Th_231": 1.063*8.64E4, "Th_232": 1.40e10*3.1536E7, "Th_233": 21.83*60,
     "Pa_231": 3.28E4*3.1536E7, "Pa_232": 1.32*8.64E4, "Pa_233": 26.967*8.64E4,
     "U_232": 69.8*3.1536E7, "U_233": 1.592E5*3.1536E7, "U_234": 2.46E5*3.1536E7, 
@@ -21,19 +20,23 @@ hl = {#Contains the half lives of varius isotopes.
     "Pu_241": 14.29*3.1536E7, "Pu_242": 3.75E5*3.1536E7, "Pu_243": 4.956*3600, 
     "Am_241": 432.7*3.1536E7, "Am_242_metastable": 141*3.1536E7, "Am_242": 16.02*3600,
     "Am_243": 7.37E3*3.1536E7, "Am_244": 10.1*3600,
-    "Cm_242": 162.8*8.64E4,"Cm_243": 29.1*3.1536E7,"Cm_244": 18.1*3.1536E7}
-   
-molwe = {#Contains the atomic numbers of the respective isotopes (used to convert between mass and number) 
+    "Cm_242": 162.8*8.64E4,"Cm_243": 29.1*3.1536E7,"Cm_244": 18.1*3.1536E7
+    }
+
+#Contains the atomic numbers of the respective isotopes (used to convert between mass and number)    
+molwe = {
     "Th_231": 231, "Th_232": 232, "Th_233": 233, 
     "Pa_231": 231, "Pa_232": 232, "Pa_233": 233,
     "U_232": 232, "U_233": 233, "U_234": 234, "U_235": 235, "U_236": 236, "U_237": 237, "U_238": 238, "U_239": 239,
     "Np_237": 237, "Np_238": 238, "Np_239": 239, 
     "Pu_238": 238, "Pu_239": 239, "Pu_240": 240, "Pu_241": 241, "Pu_242": 242, "Pu_243": 243,
     "Am_241": 241, "Am_242_metastable": 242, "Am_242": 242, "Am_243": 243, "Am_244": 244,
-    "Cm_242": 242, "Cm_243": 243, "Cm_244": 244}
+    "Cm_242": 242, "Cm_243": 243, "Cm_244": 244
+    }
    
 #Cross Section Data from Brookhaven National Laboratory Website.
-xs = {#Contains the cross sections of varius isotopes when incident neutrons are at 0.0253 ev.
+#Contains the cross sections of varius isotopes when incident neutrons are at 0.0253 ev.
+xs = {
     "Th_231": {"capture": 1630.76,"fission": 40.0085}, 
     "Th_232": {"capture": 7.37062,"fission": 0}, 
     "Th_233": {"capture": 1290.6,"fission": 15.0059}, 
@@ -64,11 +67,13 @@ xs = {#Contains the cross sections of varius isotopes when incident neutrons are
     "Am_244": {"capture": 600.162,"fission": 2300.74}, 
     "Cm_242": {"capture": 19.2054,"fission": 4.68339}, 
     "Cm_243": {"capture": 131.96,"fission": 589.769}, 
-    "Cm_244": {"capture": 15.2818,"fission": 1.02554}}
+    "Cm_244": {"capture": 15.2818,"fission": 1.02554}
+    }
    
 #None type used when isotope is not considered, not shown in figures 12.2 and 12.3
 #Tranmutation Modes    
-tm = {"Th_231": {"decay": "Pa_231", "capture": "Th_232"}, 
+tm = {
+    "Th_231": {"decay": "Pa_231", "capture": "Th_232"}, 
     "Th_232": {"decay": None, "capture": "Th_233"}, 
     "Th_233": {"decay": "Pa_233", "capture": None}, 
     "Pa_231": {"decay": None, "capture": "Pa_232"},
@@ -98,78 +103,105 @@ tm = {"Th_231": {"decay": "Pa_231", "capture": "Th_232"},
     "Am_244": {"decay": "Cm_244", "capture": None}, 
     "Cm_242":{"decay": "Pu_238", "capture": "Cm_243"}, 
     "Cm_243": {"decay": "Pu_239", "capture": "Cm_244"}, 
-    "Cm_244": {"decay": "Pu_240", "capture": None}}
+    "Cm_244": {"decay": "Pu_240", "capture": None}
+    }
 
 #Matrix Order
-mo = ["Th_231", "Th_232", "Th_233", 
+mo = [
+      "Th_231", "Th_232", "Th_233", 
       "Pa_231", "Pa_232", "Pa_233",
       "U_232", "U_233", "U_234", "U_235", "U_236", "U_237", "U_238", "U_239",
       "Np_237", "Np_238", "Np_239", 
       "Pu_238", "Pu_239", "Pu_240",  "Pu_241", "Pu_242", "Pu_243",
       "Am_241", "Am_242_metastable", "Am_242", "Am_243", "Am_244",
-      "Cm_242", "Cm_243", "Cm_244"] 
+      "Cm_242", "Cm_243", "Cm_244"
+      ] 
 
 def number(isotope, mass_0, flux, time):
-    #Calculates the initial amount of atoms of the listed isotope given mass
-    no = [0]*31
+    """
+    Given mass of a pure isotope, calculates the number of atoms of all isotopes
+    over time.
+    """
+    
+    #Column vector containing original number.
+    no = [0]*len(mo)
     atomic_mass = molwe[isotope]
     Na = 0.6022E24
     N_0 = mass_0*(Na)/(atomic_mass)
     no[mo.index(isotope)] = N_0
-    M = np.zeros((len(mo),len(mo)))#Creates a nxn matrix where n is the number of considered isotopes
+    
+    #Creates a nxn matrix where n is the number of considered isotopes.    
+    M = np.zeros((len(mo),len(mo)))
     for i in range(0,len(mo)):
-        #Creates the diagonal of the coefficient matrix of N, used to solve matrix*N = dN/dt
         M[i,i] = -np.log(2)/hl[mo[i]] - flux*(xs[mo[i]]["fission"]+xs[mo[i]]["capture"])*1E-24
         if tm[mo[i]]["decay"] != None:
             if len(tm[mo[i]]["decay"]) != 2:
                 M[mo.index(tm[mo[i]]["decay"]), i] = np.log(2)/hl[mo[i]]
-            else:#Americium 242 decays two different ways
+            else:  #Americium 242 decays two different ways
                 M[mo.index(tm[mo[i]]["decay"]["mode1"]), i] = 0.83*np.log(2)/hl[mo[i]]
                 M[mo.index(tm[mo[i]]["decay"]["mode2"]), i] = 0.17*np.log(2)/hl[mo[i]]
         if tm[mo[i]]["capture"] != None:
             if len(tm[mo[i]]["capture"]) != 2:
                 M[mo.index(tm[mo[i]]["capture"]), i] = flux*(xs[mo[i]]["capture"])*1E-24
-            else:#Americium 241 captures into a metastable and an unstable isotope
+            else:  #Americium 241 captures into a metastable and an unstable isotope
                 M[mo.index(tm[mo[i]]["capture"]["mode1"]), i] = 0.11*flux*(xs[mo[i]]["capture"])*1E-24
                 M[mo.index(tm[mo[i]]["capture"]["mode2"]), i] = 0.89*flux*(xs[mo[i]]["capture"])*1E-24   
-    def y_prime(y, time, M):#Solves the set of differential equations contained in the matrix M
+    
+    #Solves the system of differential equations housed within the matrix.
+    def y_prime(y, time, M):
         return(np.dot(M, y))
     sol = odeint(y_prime, no, time, args=(M,))
     return(sol)
+
+
 def fission_rate(isotope, mass_0 = 1E-6, flux = 1E13, endtime = 3.1536E7):
-    """Plots the fission rates for given initial conditions over a specified time."""
+    """
+    Plots the fission rates for given initial conditions over a specified time.
+    """
+    
     time = np.linspace(0, endtime, 100)
     sol = number(isotope, mass_0, flux, time)
-    for i in range(0, len(time)): #Converts the matrix containing number of atoms to fission rates.
+    
+    #Converts the values in the matrix from number of atoms to fission rates.
+    for i in range(0, len(time)): 
         for j in range(0,len(sol[0])):
             sol[i][j] = sol[i][j]*flux*xs[mo[j]]["fission"]*10**-24
-    fission = []    #Contains the fission contribution of each isotope at one time interval.
-    fissionsum = [] #Contains the total fission contribution of all isotopes at each time interval.
+            
+    #Creates a column vector containing the total fission rate at each time interval.       
+    fission = []
+    fissionsum = []  
     for i in range(0,len(time)):
         for j in range(0,len(sol[0])):
             fission.append(sol[i][j])
         fissionsum.append(sum(fission))
         fission = []
+
     #Will apply a legend entry if the isotope ever contributes at least 5% to total fission.
-    sf = []#Stores Isotopes order number for the list "mo" if the above condition is met.
+    sf = []
     for i in range(0,len(time)):
         for j in range(0,len(sol[0])):
             if sol[i][j] > 0.05*fissionsum[i]:
                 sf.append(j)
     sf = list(set(sf))
-    sol = sol[:,sf]#Deletes isotopes from the array if the 5% condition is not met.
-    other = []     #Sums all isotope contributions that do not meet the 5% condition.
+    sol = sol[:,sf]
+    
+    #Sums all isotope contributions that do not meet the 5% condition.
+    other = []     
     for i in range(0,len(time)):
-        temp = []  #Temporarily contains the element of the list "other" for each time interval.
+        temp = [] 
         for j in range(0,len(sol[0])):
             temp.append(sol[i][j])
             temp = [sum(temp)]
         other.append(fissionsum[i]-temp[0])   
-    legend = []    #Contains the titles that will be used in the legend
-    for i in sf:   #Adds in the isotope names that met the 5% condition
+        
+    #Creates a list containing names that will be displayed in the legend.    
+    legend = []   
+    for i in sf:  
         legend.append(mo[i])
     legend.append("Other")
     legend.append("Total")
+    
+    #Plots fission rates vs. time.
     plot = plt.plot(time, sol,time,other,time,fissionsum,"k--")
     plt.legend(plot,legend,loc="center left",bbox_to_anchor=(1.0,0.5))
     plt.xlabel("Time(s)")
@@ -178,15 +210,22 @@ def fission_rate(isotope, mass_0 = 1E-6, flux = 1E13, endtime = 3.1536E7):
     plt.yscale("log")
     plt.show()
     plt.close()
+    
 def mass(isotope, mass_0 = 1E-6, flux = 1E13, endtime = 3.1536E7):
-    #Calculates the mass (in micrograms) of each isotope for given initial conditions over time
+    """
+    Calculates the mass (in micrograms) of each isotope for given initial 
+    conditions over a specified time.
+    """
+    
     time = np.linspace(0, endtime, 100)
     sol = number(isotope, mass_0, flux, time)
     Na = 0.6022E24
     for i in range(0,len(time)):
         for j in range(0,len(sol[0])):
             sol[i][j] = sol[i][j]*molwe[mo[j]]/Na*10**6
-    sm = []#Sufficient mass condition used to discriminate against isotopes of negligible mass
+            
+    #Sufficient mass condition, 0.01 microgram and below discriminated out of plot.
+    sm = []
     for i in range(0,len(time)):
         for j in range(0,len(sol[0])):
             if sol[i][j] > 0.01:
@@ -196,6 +235,8 @@ def mass(isotope, mass_0 = 1E-6, flux = 1E13, endtime = 3.1536E7):
     for i in sm:
         legend.append(mo[i])
     sol = sol[:,sm]
+    
+    #Plots mass vs. time.
     fig = plt.plot(time, sol)
     plt.xlabel("Time(s)")
     plt.ylabel("Mass(microgram)")
